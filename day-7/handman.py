14e57f8da30924ca5cbf9733905e810e9f64321a
HowManyShrimp <kim_yonghui@outlook.com>
2023-12-18 14:12:45 -0500

    Update handman.py

--- day-7/handman.py
+++ day-7/handman.py
@@ -1,1 +1,53 @@
+"""
+I don't know where I left the hangman words and art. I'll find them and add them eventually
+"""
 
+import random
+# import hangman_art
+# import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+# print(hangman_art.logo)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+guesses = []
+while not end_of_game:
+    print(f"guesses: {' '.join(guesses)}")
+    guess = input("Guess a letter: ").lower()
+    guesses.append(guess)
+    if guess in display:
+        print(f"You already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+
+    if guess not in chosen_word:
+        print(f"You guess {guess}, that is wrong.")
+        lives -= 1
+        if lives == 0:
+          end_of_game = True
+          print("You lose")
+            
+
+    print(f"{''.join(display)}")
+  
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
