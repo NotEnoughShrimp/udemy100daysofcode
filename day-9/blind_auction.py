7cdc71fab57f4646051ea4f00be84ea3dc8c9f39
HowManyShrimp <kim_yonghui@outlook.com>
2023-12-18 14:14:30 -0500

    Update blind_auction.py

--- day-9/blind_auction.py
+++ day-9/blind_auction.py
@@ -1,1 +1,31 @@
+import os
+import art
 
+# print(art.logo)
+
+buyer_info_list = []
+while True:
+    buyer_info = {}
+    buyer_name = input("Enter name: ")
+    buyer_bid = int(input("Enter bid: $"))
+    buyer_info = {"name": buyer_name, "bid": buyer_bid}
+    buyer_info_list.append(buyer_info)
+    
+    restart = input("Anyone else? y/n\n> ")
+    if restart == 'y':
+        os.system('cls')
+        continue
+    else:
+        break
+
+# bid_value = []
+highest_bid = None
+winner_name = None
+for buyer_info in buyer_info_list:
+    bid = buyer_info['bid']
+    if highest_bid is None or bid > highest_bid:
+        highest_bid = bid
+        winner_name = buyer_info['name']
+
+    
+print(f"${highest_bid} wins it. This goes to {winner_name.title()}")
