e3f4780775a814ee12f9d0345182f42e3a48ac82
HowManyShrimp <kim_yonghui@outlook.com>
2024-03-12 13:38:47 -0400

    Create data.py

---
+++ day-17-quiz-game/data.py
@@ -0,0 +1,129 @@
+# question_data = [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#              "you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+#      "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
+# question_data = [
+#     {"text": "In the game Melty Blood Actress Again Current Code, you can enter Blood "
+#              "Heat mode in Half Moon style.",
+#      "answer": "False"},
+#     {"text": "The video game Fuel has an open world that is 5,560 square miles?",
+#      "answer": "True"},
+#     {"text": "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
+#      "answer": "False"},
+#     {"text": "The names of Roxas&#039;s Keyblades in Kingdom Hearts are Oathbreaker & Oblivion.",
+#      "answer": "True"},
+#     {"text": "In Undertale, having a Fun Value set to 56-57 will play the Wrong Number"
+#              "Song Call.",
+#      "answer": "False"},
+#     {"text": "In The Witcher 3, the Zoltan Chivy Gwent card can be found under the Hanged Man&#039;s Tree.",
+#      "answer": "True"},
+#     {"text": "In &quot;Minecraft&quot;, gold tools are faster than diamond tools.", "answer": "True"},
+#     {"text": "In &quot;The Sims&quot; series, the most members in a household you can have is 8.",
+#      "answer": "True"},
+#     {"text": "TF2: Sentry rocket damage falloff is calculated based on the distance between the sentry and "
+#              "the enemy, not the engineer and the enemy",
+#      "answer": "False"},
+#     {"text": "The first &quot;Metal Gear&quot; game was released for the PlayStation 1.", "answer": "False",
+#      }]
+
+# https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=boolean - hard
+#
+
+question_data = [
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "In the game &quot;Melty Blood Actress Again Current Code&quot;, you can enter Blood Heat "
+                    "mode in Half Moon style.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "The video game &quot;Fuel&quot; has an open world that is 5,560 square miles?",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "The names of Roxas&#039;s Keyblades in Kingdom Hearts are &quot;Oathkeeper&quot; "
+                    "and &quot;Oblivion&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "In Undertale, having a &quot;Fun Value&quot; set to 56-57 will play the &quot;Wrong "
+                    "Number Song Call&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "In The Witcher 3, the Zoltan Chivay Gwent card can be found under the Hanged Man&#039;s Tree.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "In &quot;Minecraft&quot;, gold tools are faster than diamond tools.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "In &quot;The Sims&quot; series, the most members in a household you can have is 8.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "TF2: Sentry rocket damage falloff is calculated based on the distance between the sentry "
+                    "and the enemy, not the engineer and the enemy",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Video Games",
+        "question": "The first &quot;Metal Gear&quot; game was released for the PlayStation 1.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
